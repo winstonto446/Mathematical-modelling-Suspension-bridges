203abb934b45c0f0685c97667a9a3df67936a45f
winstonto446 <winstonto446@gmail.com>
2025-12-28 03:28:43 +0000

    Refactor comments in suspensionbridge.py
    Updated comments for clarity and consistency in the suspension bridge calculations.

--- suspensionbridge.py
+++ suspensionbridge.py
@@ -13,17 +13,17 @@ class SuspensionBridge_x(SuspensionBridgeBase):
         """
         n =  self.N-2
         A = np.zeros([n,n], dtype='float64')                              
-        #create a (N-2)X(N-2) matrix with entries 0
+
         for i in range(1,n):                                              
-        #create a loop to set the corresponding values in matrix A
-            #if (j==i), then delta_{i,j} = 1
+        # create a loop to set the corresponding values in matrix A
+            # if (j==i), then delta_{i,j} = 1
             A[i-1,i-1] = -self.Ksc/self.M*(np.cos(self.thetas[i])**2 \
                           +np.cos(self.thetas[i+1])**2)                   
-            #if (j==i+1), then delta_{i,j} = 1
+            # if (j==i+1), then delta_{i,j} = 1
             A[i-1,i] = self.Ksc/self.M*np.cos(self.thetas[i+1])**2      
-            #if (j==i-1), then delta_{i,j} = 1
+            # if (j==i-1), then delta_{i,j} = 1
             A[i,i-1] = self.Ksc/self.M*np.cos(self.thetas[i+1])**2        
-        #set the entry in A for the last term where i == j == N-2
+        # set the entry in A for the last term where i == j == N-2
         A[n-1,n-1] = -self.Ksc/self.M*(np.cos(self.thetas[n])**2 \
                         + np.cos(self.thetas[n+1])**2)                    
         return(A)
@@ -38,18 +38,17 @@ class SuspensionBridge_z(SuspensionBridge_x):
            vertical displacements of the bridge
         """
         n =  self.N-2
-        A = np.zeros([n,n], dtype='float64') 
-        #create a (N-2)X(N-2) matrix with entries 0                          
+        A = np.zeros([n,n], dtype='float64')                     
         for i in range(1,n):                                           
-        #create a loop to set the corresponding values in matrix A
-            #if (j==i), then delta_{i,j} = 1
+        # create a loop to set the corresponding values in matrix A
+            # if (j==i), then delta_{i,j} = 1
             A[i-1,i-1] = -self.Ksc/self.M*(np.sin(self.thetas[i])**2 \
                             + np.sin(self.thetas[i+1])**2)              
-            #if (j==i+1), then delta_{i,j} = 1
+            # if (j==i+1), then delta_{i,j} = 1
             A[i-1,i] = self.Ksc/self.M*np.sin(self.thetas[i+1])**2    
-            #if (j==i-1), then delta_{i,j} = 1 
+            # if (j==i-1), then delta_{i,j} = 1 
             A[i,i-1] = self.Ksc/self.M*np.sin(self.thetas[i+1])**2      
-        #set the entry in A for the term where i == j == N-2
+        # set the entry in A for the term where i == j == N-2
         A[n-1,n-1] = -self.Ksc/self.M*(np.sin(self.thetas[n])**2 \
                         + np.sin(self.thetas[n+1])**2)                  
         
@@ -73,7 +72,7 @@ class SuspensionBridge_xz(SuspensionBridge_x):
 
         for i in range(1,n): # we do not compute A for end nodes
           # Array A must start with index 0 -> shift index by 1
-          #if (i==j), then delta_{i,j}=1
+          # if (i==j), then delta_{i,j}=1
             A[i-1,i-1] = -(np.cos(self.thetas[i])**2+np.cos(self.thetas[i+1])**2)  
             
             A[n+i-1,n+i-1] = -(np.sin(self.thetas[i])**2+np.sin(self.thetas[i+1])**2) -k
@@ -82,19 +81,19 @@ class SuspensionBridge_xz(SuspensionBridge_x):
             A[n+i-1,i-1] = -np.cos(self.thetas[i])*np.sin(self.thetas[i])\
                       -np.cos(self.thetas[i+1])*np.sin(self.thetas[i+1])
         
-          #if (i+1 == j), then delta_{i,j}=1
+          # if (i+1 == j), then delta_{i,j}=1
             A[i-1,i] = np.cos(self.thetas[i+1])**2                                 
             A[n+i-1,n+i] = np.sin(self.thetas[i+1])**2 +0.5*k 
             A[i-1,n+i] = np.cos(self.thetas[i+1])*np.sin(self.thetas[i+1])
             A[n+i-1,i] = np.cos(self.thetas[i+1])*np.sin(self.thetas[i+1])
             
-          #if (i-1 == j), then delta_{i,j}=1 
+          # if (i-1 == j), then delta_{i,j}=1 
             A[i,i-1] = np.cos(self.thetas[i+1])**2                                 
             A[n+i,n+i-1] = np.sin(self.thetas[i+1])**2  +0.5*k  
             A[i,n+i-1] = np.cos(self.thetas[i+1])*np.sin(self.thetas[i+1])
             A[n+i,i-1] = np.cos(self.thetas[i+1])*np.sin(self.thetas[i+1])
         
-        #set the entries in A for the term where i == j == N-2
+        # set the entries in A for the term where i == j == N-2
         A[n-1,n-1] = -(np.cos(self.thetas[n])**2+np.cos(self.thetas[n+1])**2)      
         # n starts at correct place                                                                     
         A[n+n-1,n+n-1] = -(np.sin(self.thetas[n])**2+np.sin(self.thetas[n+1])**2) -k
